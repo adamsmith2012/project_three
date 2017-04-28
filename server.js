0f9fe6af4de45fab2ac759fc0bb45cd7d04f5921
588870 <adamsmith@c1mrq060hf4h.attlocal.net>
2017-04-28 14:52:20 -0700

    Initial project setup

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+/****** MODELS ******/
+
+// Heroku / Localhost setup
+
+var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/got'
+
+/****** MIDDLEWARE ******/
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
+
+/****** CONTROLLERS ******/
+
+// var controller = require('./controllers/{file}.js');
+// app.use('/{url}', ontroller);
+
+/****** LISTENERS ******/
+
+app.get('/', function(req, res) {
+  res.render('index.ejs', {
+    //currentUser: req.session.currentUser
+  });
+});
+
+mongoose.connect(mongoDBURI);
+mongoose.connection.once('open', function() {
+  console.log("Connected to mongo");
+});
+
+app.listen(port, function() {
+  console.log("Listening on port " + port);
+});
