2c4d0734d36412bba43b3ddb1d09aa9500574e07
Soniya Jassal <jassal_soniya@bah.com>
2017-05-02 21:37:03 -0400

    Html template in place ready to connect angular

--- public/js/app.js
+++ public/js/app.js
@@ -11,4 +11,15 @@ app.controller('ThroneController', ['$http', function($http){
       })
 
    }]);
-   
+
+//Enabling one page routing for landing page-tentative
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+   $locationProvider.html5Mode({enabled:true});
+   $routeProvider.when('/url1', { //route would come from controller file with routes
+      template: '<h2>This is the the URL1 Section</h2>',
+      controller: function(){
+         this.foo = 'bar';
+      },
+      controllerAs: 'main'
+   });
+}]);
